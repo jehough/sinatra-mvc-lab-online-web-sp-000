1c90b9ec0dc1b2c85e79ac9a2e2a2bc81fd8e36d
Jim Hough <teachlife@outlook.com>
2019-06-24 15:51:00 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,19 +1,23 @@
 class PigLatinizer
   attr_accessor :text
 
-  def piglatinize(input)
-      vowels = %w{a e i o u}
-      input.gsub(/(\A|\s)\w+/) do |str|
-              str.strip!
-          while not vowels.include? str[0] or (str[0] == 'u' and str[-1] == 'q')
-              str += str[0]
-              str = str[1..-1]
-          end
-          if vowels.include? str[-1]
-            str= ' ' + str + 'way'
-          else
-            str  = ' ' + str + 'ay'
-          end
-      end.strip
+  def piglatinize (phrase)
+    alpha = ('a'..'z').to_a
+    vowels = %w[a e i o u]
+    consonants = alpha - vowels
+    arr = phrase.split
+    translated = arr.map do |str|
+      if vowels.include?(str[0])
+        str + 'way'
+      elsif consonants.include?(str[0]) && consonants.include?(str[1])
+        str[2..-1] + str[0..1] + 'ay'
+      elsif consonants.include?(str[0])
+        str[1..-1] + str[0] + 'ay'
+      else
+        str # return unchanged
+      end
+    end
+    translated.join(' ')
+  end
   end
 end
