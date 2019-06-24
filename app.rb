070147ed24c955f78c1f0d156cff81208e6ed744
Jim Hough <teachlife@outlook.com>
2019-06-24 15:24:09 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/user_input' do
+    erb :user_input
+  end
+
+  post '/piglatinize' do
+    pig_latin = PigLatinizer.new
+    @translated = pig_latin.piglatinize(params[:text])
+    erb :results
+  end
 end
